4d0a07651363987b346270c13003e763818ecb3c
techngparames <techngparames@gmail.com>
2026-08-01 12:54:33 +0530

    Update Oops.py

--- Oops.py
+++ Oops.py
@@ -3,7 +3,7 @@ class Kerala:
     drink="Tender Coconut"
 
     def temple(self):
-        print("I Love To go for Temple ,Because it give some peace")
+        print("I Love To go for Temple ,Because it give some peace..")
     def beach(self):
         print('I Love Beach')
 
